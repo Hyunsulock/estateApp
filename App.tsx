14ac43fb096007a45ddbc75189f5eeffe5caf9de
Hyunsulock <hateibs1@gmail.com>
2024-09-09 21:58:02 +0900

    d
    d

--- App.tsx
+++ App.tsx
@@ -3,6 +3,6 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  return <Redirect href={'/sign-in'}
+  return <Redirect href={'/sign-in'}/>
 }
 
